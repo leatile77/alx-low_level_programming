9dc25ee7909aca370d50a44c94618f6b712b6497
leatile77 <moilaleatile@gmail.com>
2023-09-09 01:39:12 -0700

    bonus

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,14 +13,14 @@ for (a = '0'; a < '9'; a++)
 {
 for (b = '0'; b <= '9'; b++)
 {
-if (b > a)
+if (b > a && (b != '9' && a != '8'))
 {
 putchar(a);
 putchar(b);
-}
-}
 putchar(',');
 putchar(' ');
+}
+}
 }
 return (0);
 }
