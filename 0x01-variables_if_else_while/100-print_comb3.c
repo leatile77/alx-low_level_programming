baccb1181f08f785e9681dcb15e310d157a08ada
leatile77 <moilaleatile@gmail.com>
2023-09-09 00:56:44 -0700

    bonus

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - two digits
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int a, b;
+for (a = '0'; a < '9'; a++)
+{
+for (b = '0'; b <= '9'; b++)
+{
+if (b > a)
+{
+putchar('a');
+putchar('b');
+putchar(',');
+putchar(' ');
+}
+}
+
+}
+
+
+return (0);
+}
