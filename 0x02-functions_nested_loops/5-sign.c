c13786df0cdd96a46f1a48bbac315dec10812bd7
leatile77 <moilaleatile@gmail.com>
2023-09-12 09:52:33 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * n - is n less or greater than zero
+ *
+ * Return: 0 or 1
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+_putchar('+');
+}
+
+else if (n == 0)
+{
+return (0);
+_putchar('0');
+}
+
+else
+{
+return (-1);
+_putchar('-');
+}
+}
