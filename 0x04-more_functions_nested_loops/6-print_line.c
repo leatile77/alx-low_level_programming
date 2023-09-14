37c76daa58980ed085803f9a0d12346b58a8c9f1
leatile77 <moilaleatile@gmail.com>
2023-09-14 14:23:02 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,18 +8,18 @@
 
 void print_line(int n)
 {
-if (n > 0)
+if (n <= 0)
+{
+_putchar('\n');
+}
+
+else
 {
 int i;
 for (i = 0; i < n; i++)
 {
 _putchar('_');
 }
-}
-
-else
-{
-_putchar('\n');
 }
 
 return (0);
