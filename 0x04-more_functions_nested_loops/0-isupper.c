4507a2ef96f5651110186e6292366ef36f43feec
leatile77 <moilaleatile@gmail.com>
2023-09-14 07:04:24 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks if
+ * @c is in upper case
+ * Return: 0 if lowercase, 1 if uppercase
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && C <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+}
