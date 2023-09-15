8188d038ffe5fd01b096f80ee4961310b241b66c
leatile77 <moilaleatile@gmail.com>
2023-09-15 15:16:10 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,14 +10,11 @@ void print_most_numbers(void)
 int i;
 for (i = 0; i < 10; i++)
 {
-if (i != 2 || i != 4)
+if (i != 2 && i != 4)
 {
-_putchar('i');
+_putchar(i + '0');
 }
-else
-{
-break;
-}
+
 }
 _putchar('\n');
 }
