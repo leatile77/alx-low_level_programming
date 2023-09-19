eaa2723ec7bd7353b45f2a35b378b3c3e6803ec1
leatile77 <moilaleatile@gmail.com>
2023-09-19 03:16:07 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,20 +16,20 @@ while (str[j] != '\0')
 j++;
 }
 
-if (j % 2 == 0)
+if (j % 2 == 1)
 {
-for (n = j / 2; n <= j; n++)
-{
-_putchar(str[n]);
-}
+n = (j - 1) / 2;
+n++;
 }
 
-else if (j % 2 != 0)
+else
 {
-for (n = (j - 1) / 2; n <= j; n++)
+n = j / 2;
+}
+for (; n < j; n++)
 {
 _putchar(str[n]);
 }
-}
+
 _putchar('\n');
 }
