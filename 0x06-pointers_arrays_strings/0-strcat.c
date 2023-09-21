ddcb15a3071541ef8aef07b0ed17f7f77dfd53d7
leatile77 <moilaleatile@gmail.com>
2023-09-21 03:35:43 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - Function that concatenates src into dest
+ * @dest: destination char pointer
+ * @src: source char pointer
+ *
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int i, j;
+for (i = 0; dest[i] != '\0'; i++)
+{
+;
+}
+
+j = -1;
+do
+{
+j++;
+dest[i] = src[j];
+i++;
+}
+while (src[j] != '\0');
+
+return (dest);
+}
