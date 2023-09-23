1242112bdc4e664a7a72a2675d963c2cd6f8ff74
leatile77 <moilaleatile@gmail.com>
2023-09-22 20:26:56 -0700

    task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Function that prints a chess board
+ * @a: array fo chess
+ * @8: Size of board
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+_putchar(a[i][j]);
+}
+_putchar('\n');
+}
+}
