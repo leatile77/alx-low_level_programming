5ef3f04c3f0cc7a0c14d4b265d4b5078029447ad
leatile77 <moilaleatile@gmail.com>
2023-09-29 17:32:06 -0700

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[])
+{
+int i, add;
+
+if (argc < 2)
+{
+printf("0\n");
+}
+
+for (i = 1; i < argc; i++)
+{
+if (*argv[i] < 48 || *argv[i] > 57)
+{
+printf("Error\n");
+return (1);
+}
+
+add += atoi(argv[i]);
+}
+printf("%d\n", add);
+
+return (0);
+}
