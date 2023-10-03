12a6ae47759afa015a3aa8228129b007bc9d68db
leatile77 <moilaleatile@gmail.com>
2023-10-03 06:48:50 -0700

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *str_concat(char *s1, char *s2)
+{
+
+char *conct;
+int i, n, x, k;
+if (s1 && s2 == NULL)
+{
+s1 = "";
+s2 = "";
+}
+
+i = 0;
+while (s1[i])
+{
+i++;
+}
+
+n = 0;
+while (s2[n])
+{
+n++;
+}
+
+x = n + i;
+
+conct = malloc((x * sizeof(char)) + 1);
+
+if (conct == NULL)
+{
+return (NULL);
+}
+
+n = 0;
+for (k = 0; k < x; k++)
+{
+
+if (k <= i)
+{
+conct[k] = s1[k];
+}
+
+
+if (k >= i)
+{
+conct[k] = s2[n];
+n++;
+}
+
+}
+conct[k] = '\0';
+return (conct);
+
+}
