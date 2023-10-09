7aff0282cc1de749da0434c9644ea2cb09ef5006
leatile77 <moilaleatile@gmail.com>
2023-10-09 15:15:44 -0700

    define structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Structure to define
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Name of owner of the dog
+ */
+
 struct dog
 {
 char *name;
