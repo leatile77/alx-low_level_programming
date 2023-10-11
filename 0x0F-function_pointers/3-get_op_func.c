18f6eb6bcbf3ebd29401df6a18d8c9daa7487ce5
leatile77 <moilaleatile@gmail.com>
2023-10-11 14:34:35 -0700

    task 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "calc.h"
+
+/**
+ * get_op_func - Function that selsects the correct operation
+ * @s: Operator passed as a char
+ *
+ * Return: Pointer to operators function
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i;
+i = 0;
+
+while (ops[i].op != NULL && *(ops[i].op) != *s)
+{
+i++;
+}
+return (ops[i].f);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,8 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+
+}
