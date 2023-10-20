3f008bd5e13ea4e3f166c530397af1462fd2db56
leatile77 <moilaleatile@gmail.com>
2023-10-19 18:16:44 -0700

    print this

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * first - Function that Prints you are beat
+ *
+ * Return: void
+ */
+
+void first(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
