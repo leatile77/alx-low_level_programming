fc62dd484110bdefc287da3f61e088eb580e0518
leatile77 <moilaleatile@gmail.com>
2023-10-24 19:24:09 -0700

    Quick bug fix

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,16 +2,16 @@
 
 
 /**
+ * listint_len - Function that returns number of elements in list
+ * @h: Our linked list
  *
- *
- *
- *
+ * Return: number of elements in list
  */
 
 size_t listint_len(const listint_t *h)
 {
 
-int i;
+size_t i;
 
 for (i = 0; h; i++)
 {
