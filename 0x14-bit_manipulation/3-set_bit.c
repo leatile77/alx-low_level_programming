64748fda37589cb54a59811cea60c99f044a4cec
leatile77 <moilaleatile@gmail.com>
2023-10-27 07:54:33 -0700

    Change nth bitwq value

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * set_bit - Function that sets the value of a bit to 1 at index
+ * @n: binary to change at indexth position to 1
+ * @index: Position to change to 1
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+if (index > 63)
+{
+return (-1);
+}
+
+*n = ((1UL << index) | *n);
+return (1);
+}
