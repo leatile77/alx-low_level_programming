bb9d20cee7b89ff91993a3a8310a17c051ef40f8
leatile77 <moilaleatile@gmail.com>
2023-10-30 07:18:46 -0700

    read and print from file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * read_textfile - Function to read and print a file
+ * filename - file to print
+ * letters - Nummber of chars to print from filename
+ *
+ * Return: 0 or number of printed chars
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+ssize_t r, w;
+char *print;
+int o;
+o = 0;
+
+if (!filename)
+{
+return (0);
+}
+
+o = open(filename, O_RDONLY);
+
+if (o == -1)
+{
+return (0);
+}
+
+print = malloc(sizeof(char) * letters);
+
+if (print == NULL)
+{
+return (0);
+}
+
+r = read(o, print, letters);
+w = write(STDOUT_FILENO, print, r);
+
+free(print);
+
+close(o);
+return (w);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+15 - Input Output Files
