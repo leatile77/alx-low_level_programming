e7319cc7fe768f967e97717d9a849d5f6510aed0
leatile77 <moilaleatile@gmail.com>
2023-12-07 20:17:41 +0200

    free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+
+
+void free_dlistint(dlistint_t *head)
+{
+if (head == NULL)
+{
+return;
+}
+
+while (head->next)
+{
+head = head->next;
+free(head->prev);
+}
+free(head);
+}
