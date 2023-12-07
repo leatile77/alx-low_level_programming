db38662831479f62e636fb25398c757ecfc3100c
leatile77 <moilaleatile@gmail.com>
2023-12-07 20:06:10 +0200

    add node at end

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+dlistint_t *new, *tmp;
+
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+{
+return (NULL);
+}
+
+new->n = n;
+new->next = NULL;
+tmp = *head;
+
+if (tmp != NULL)
+{
+while (tmp->next != NULL)
+{
+tmp = tmp->next;
+}
+tmp->next = new;
+}
+else
+{
+*head = new;
+}
+new->prev = tmp;
+
+return (new);
+}
