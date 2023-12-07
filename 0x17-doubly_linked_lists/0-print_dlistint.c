e70d940fb9e0ceef28c2ff91d9b66c9b05ec76c3
leatile77 <moilaleatile@gmail.com>
2023-12-07 18:24:44 +0200

    print int in list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,18 +2,27 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * print_dlistint_t - Function that prints int data in a list
+ * @h: list to print data from
+ *
+ * Return: number of nodes
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
   int num;
   num = 0;
-  if (!h)
+  if (h == NULL)
     {
-      return (0);
+      return (num);
     }
 
   while (h->prev != NULL)
-    h = h->prev;
-  
+    {
+      h = h->prev;
+    }
+      
   while (h != NULL)
     {
       printf("%d\n", h->n);
