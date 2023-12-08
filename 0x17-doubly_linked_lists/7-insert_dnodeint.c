2120b631c271b3366e27df9e7b7366f646b8d13f
leatile77 <moilaleatile@gmail.com>
2023-12-08 16:56:09 +0200

    insert node at any posiion

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,41 +1,48 @@
 #include "lists.h"
 
-
+/**
+ * insert_dnodeint_at_index - Function that inserts node at index
+ * @h: list to add node in
+ * @idx: index to insert node
+ * @n: data to insert at position
+ *
+ * Return: address of new node, or NULL on failure
+ */
 
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-  dlistint_t *prv, *nxt, *new;
-  unsigned int pos;
+dlistint_t *prv, *nxt, *new;
+unsigned int pos;
 
-  pos = 1;
-  
-  if (*h == NULL)
-    {
-      return (NULL);
-    }
+pos = 1;
 
-  new = malloc(sizeof(dlistint_t));
-  if (new == NULL)
-    {
-      return (NULL);
-    }
+if (*h == NULL)
+{
+return (NULL);
+}
 
-  prv = *h;
-  nxt = *h;
-  nxt = nxt->next;
-  while (pos != idx)
-    {
-      prv = prv->next;
-      nxt = nxt->next;
-      pos++;
-    }
-  new->n = n;
-  new->prev = prv;
-  new->next = nxt;
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+{
+return (NULL);
+}
 
-  prv->next = new;
-  nxt->prev = new;
+prv = *h;
+nxt = *h;
+nxt = nxt->next;
+while (pos != idx)
+{
+prv = prv->next;
+nxt = nxt->next;
+pos++;
+}
+new->n = n;
+new->prev = prv;
+new->next = nxt;
 
-  return (new);
+prv->next = new;
+nxt->prev = new;
+
+return (new);
 }
