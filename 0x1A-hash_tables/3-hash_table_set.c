8fc569ccc77c9a7424abfde7c04e986fdae00906
leatile77 <moilaleatile@gmail.com>
2023-12-22 22:24:49 +0200

    add new element to table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,52 @@
+#include "hash_tables.h"
+#include <string.h>
+
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+  unsigned long int ind, x;
+  hash_node_t *new;
+  int i;
+  char *value2;
+
+
+  if (ht == NULL || key == NULL || *key == '\0')
+    {
+      return (0);
+    }
+
+  value2 = strdup(value);
+  ind = key_index((const unsigned char *)key, ht->size);
+
+  for (x = ind; ht->array[x]; x++)
+    {
+      if (strcmp(ht->array[x]->key, key) == 0)
+	{
+	  free(ht->array[x]->value);
+	  ht->array[x]->value = value2;
+	  
+	  return (1);
+	}
+    }
+
+  new = malloc(sizeof(hash_node_t));
+  if (new == NULL)
+    return (0);
+
+  new->value = value2;
+  new->key = strdup(key);
+
+  for (i = ind; ht->array[i]; i++)
+    {
+      if (ht->array[i]->key == NULL)
+	{
+	  ht->array[i] = new;
+	  return (1);
+	}
+    }
+ 
+
+  ht->array[i] = new;
+  return (1);
+
+}  
